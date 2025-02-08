dfee1d4fa17416f8e346b6aac36e125daccd8a73
gaberdell <spegitthicode@gmail.com>
2025-02-08 01:55:08 -0500

    Added some basic player movement
    Some basic player movement was in fact added

---
+++ Assets/Scripts/Scrpt_Input/InputHandler.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//Code from this fantastic tutorial by SpeedTutor
+//https://www.youtube.com/watch?v=lclDl-NGUMg
+
+public class InputHandler : MonoBehaviour
+{
+    [Header("Input Action Assets")]
+    [SerializeField]
+    private InputActionAsset playerControls;
+
+    [Header("Action Map Name Reference")]
+    [SerializeField]
+    private string actionMapName = "Player";
+
+    [Header("Action Name Reference")]
+    [SerializeField]
+    private string move = "Move";
+    [SerializeField]
+    private string sprint = "Sprint";
+    [SerializeField]
+    private string attack = "Attack";
+    [SerializeField]
+    private string previous = "Previous";
+    [SerializeField]
+    private string next = "Next";
+    [SerializeField]
+    private string interact = "Interact";
+
+    [Header("Devices")]
+    [SerializeField]
+    private string mouse = "Mouse";
+
+    private InputAction moveAction;
+    private InputAction sprintAction;
+    private InputAction previousAction;
+    private InputAction nextAction;
+    private InputAction interactAction;
+
+    public Vector2 MoveInput { get; private set; }
+    public float SprintValue { get; private set; }
+    public bool PreviousTriggered { get; private set; }
+    public bool NextTriggered { get; private set; }
+    public bool InteractTriggered { get; private set; }
+
+    public bool IsMouseEnabled { get; private set; }
+
+    public static InputHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+
+        moveAction = playerControls.FindActionMap(actionMapName).FindAction(move);
+        sprintAction = playerControls.FindActionMap(actionMapName).FindAction(sprint);
+        previousAction = playerControls.FindActionMap(actionMapName).FindAction(previous);
+        nextAction = playerControls.FindActionMap(actionMapName).FindAction(next);
+        interactAction = playerControls.FindActionMap(actionMapName).FindAction(interact);
+
+        registerInputActions();
+
+        checkIfEnabledDevices();
+    }
+
+    private void checkIfEnabledDevices()
+    {
+        IsMouseEnabled = false;
+
+        foreach (InputDevice device in InputSystem.devices)
+        {
+            Debug.Log(device.name);
+            if (device.enabled && device.name == mouse)
+            {
+                
+                IsMouseEnabled = true;
+            }
+            break;
+        }
+    }
+
+    private void registerInputActions()
+    {
+        //Tricky bit of syntax but events need a function which the context is a lambda
+        //function that take a InputAction.CallbackContext and returns whatever variable type it stored
+        moveAction.performed += context => MoveInput = context.ReadValue<Vector2>();
+        moveAction.canceled += context => MoveInput = Vector2.zero;
+
+        sprintAction.performed += context => SprintValue = context.ReadValue<float>();
+        sprintAction.canceled += context => SprintValue = 0f;
+
+        previousAction.performed += context => PreviousTriggered = true;
+        previousAction.canceled += context => PreviousTriggered = false;
+
+        nextAction.performed += context => NextTriggered = true;
+        nextAction.canceled += context => NextTriggered = false;
+
+        interactAction.performed += context => InteractTriggered = true;
+        interactAction.canceled += context => InteractTriggered = false;
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        sprintAction.Enable();
+        previousAction.Enable();
+        nextAction.Enable();
+        interactAction.Enable();
+
+        InputSystem.onDeviceChange += onDeviceChange;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        sprintAction.Disable();
+        previousAction.Disable();
+        nextAction.Disable();
+        interactAction.Disable();
+
+        InputSystem.onDeviceChange -= onDeviceChange;
+    }
+
+    private void onDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        if (device.name == mouse)
+            IsMouseEnabled = !(change == InputDeviceChange.Disconnected || change == InputDeviceChange.Disabled);
+    }
+
+
+}
--- Assets/Scripts/Scrpt_Player/Scrpt_PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/Scrpt_Player/Scrpt_PlayerMovement/PlayerMovement.cs
@@ -1,29 +1,178 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [Header("Input")]
     [SerializeField]
     PlayerInput input;
 
-    void OnEnable()
-    {
-        //input.onActionTriggered += ActionComitted;
-    }
+    [SerializeField] 
+    float deadZone = 0.1f; //When we start to apply stopping friction
 
-    void OnDisable()
-    {
-        //input.onActionTriggered -= ActionComitted;
-    }
+    [Header("Speed Values")]
+    [SerializeField] 
+    float maxSpeed = 5f;
+    [SerializeField] 
+    float accel = 27f;
+    [SerializeField] 
+    float stopingAccel = 20f;
 
-    void ActionComitted(InputAction.CallbackContext callbackContext)
+    [Header("Collision")]
+    [SerializeField] 
+    CircleCollider2D plrCollider;
+    [SerializeField] 
+    Rigidbody2D rigidBody;
+
+    [Header("Rotation")]
+    [SerializeField]
+    float rotationSpeed = 360.0f;
+
+    [SerializeField]
+    float rotationAccel = 360.0f;
+
+    InputHandler inputHandler;
+
+    float previousRotationZ;
+    float goalRotationZ;
+    float moveTowardsVal;
+
+    Vector3 plrVelocity;
+
+    Vector3 previousPosition;
+    List<ContactPoint2D> contactPoints;
+
+    bool isDebugOn = false;
+    bool debugHitDeadZone = false;
+    void Start()
     {
-        Debug.Log(callbackContext.action);
+        plrCollider = plrCollider ? plrCollider : GetComponent<CircleCollider2D>();
+        rigidBody = rigidBody ? rigidBody : GetComponent<Rigidbody2D>();
+
+        contactPoints = new List<ContactPoint2D>();
+
+        previousPosition = transform.position;
+
+        inputHandler = InputHandler.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        handleMovement();
+        handleRotation();
     }
+
+    private void handleMovement()
+    {
+        Vector3 plrMoveInput = inputHandler.MoveInput;
+
+        if (plrMoveInput.magnitude > deadZone)
+        {
+            plrVelocity = Vector3.ClampMagnitude(plrVelocity + plrMoveInput * accel * Time.deltaTime, maxSpeed);
+        }
+        else
+        {
+            Vector3 slowDownVector = plrVelocity.normalized * stopingAccel * Time.deltaTime;
+
+
+            plrVelocity.x = plrVelocity.x > 0 ? Mathf.Max(plrVelocity.x - slowDownVector.x, 0f) :
+                                                    Mathf.Min(plrVelocity.x - slowDownVector.x, 0f);
+
+            plrVelocity.y = plrVelocity.y > 0 ? Mathf.Max(plrVelocity.y - slowDownVector.y, 0f) :
+                                                    Mathf.Min(plrVelocity.y - slowDownVector.y, 0f);
+        }
+
+
+        transform.position += plrVelocity * Time.deltaTime;
+    }
+
+    private void handleRotation()
+    {
+        if (inputHandler.IsMouseEnabled == true)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            goalRotationZ = Mathf.Rad2Deg * Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) + 90f;
+        }
+        else
+        {
+            goalRotationZ = plrVelocity.magnitude == 0.0f ? goalRotationZ : 90f + Mathf.Rad2Deg * Mathf.Atan2(plrVelocity.y, plrVelocity.x);
+        }
+
+        float num = Mathf.Repeat(goalRotationZ - transform.rotation.eulerAngles.z, 360f);
+        goalRotationZ = Mathf.Repeat(goalRotationZ, 360f);
+        if (num > 180f)
+        {
+            num -= 360f;
+        }
+
+        if (goalRotationZ > 180f)
+        {
+            goalRotationZ -= 360f;
+        }
+
+        if (Mathf.Repeat(Mathf.Abs(num),360f) < 1f) {
+            moveTowardsVal = 0f;
+        }
+        else
+        {
+            if (num < 0)
+            {
+                moveTowardsVal = Mathf.Max(moveTowardsVal - rotationAccel * Time.deltaTime, -rotationSpeed);
+            }
+            else
+            {
+                moveTowardsVal = Mathf.Min(moveTowardsVal + rotationAccel * Time.deltaTime, rotationSpeed);
+            }
+
+            //moveTowardsVal = Mathf.Clamp(moveTowardsVal + rotationAccel * (Mathf.Abs(num) + rotationAccel) * Time.deltaTime, -rotationSpeed, rotationSpeed);
+        }
+
+
+        float clampedCurrentVal = Mathf.Repeat(transform.rotation.eulerAngles.z, 360f);
+        if (clampedCurrentVal > 180f)
+        {
+            clampedCurrentVal -= 360f;
+        }
+
+
+        //if (moveTowardsVal > 0)
+        //{
+        bool valIsLessThanGoal = clampedCurrentVal < goalRotationZ;
+        bool hadToBeFlipped = false;
+
+        float newMoveTowardsVal = clampedCurrentVal + moveTowardsVal * Time.deltaTime;
+
+        if (newMoveTowardsVal > 180f)
+        {
+            hadToBeFlipped = true;
+            newMoveTowardsVal -= 360f;
+        }
+        if (newMoveTowardsVal < -180f)
+        {
+            hadToBeFlipped = true;
+            newMoveTowardsVal += 360f;
+        }
+
+        bool newValIsLessThanGoal = newMoveTowardsVal < goalRotationZ;
+
+        bool didNotOverShoot = (!hadToBeFlipped && valIsLessThanGoal == newValIsLessThanGoal) || (hadToBeFlipped && valIsLessThanGoal != newValIsLessThanGoal);
+
+        Debug.DrawLine(new Vector3(-1f, 0f), new Vector3(2f, 0f), valIsLessThanGoal ? Color.blue : Color.red);
+        Debug.DrawLine(new Vector3(-1f, 1f), new Vector3(2f, 1f), newValIsLessThanGoal ? Color.blue : Color.red);
+        Debug.Log(goalRotationZ);
+
+        transform.rotation = Quaternion.Euler(0f, 0f, didNotOverShoot ? newMoveTowardsVal : goalRotationZ);
+        //}
+
+        //Debug.Log(goalRotationZ);
+        //transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + moveTowardsVal * Time.deltaTime);
+
+        previousRotationZ = goalRotationZ;
+    }
+
 }
