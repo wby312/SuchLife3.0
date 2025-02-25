39bf3ce03363ed6f75959f1c0dbd7f44a4a653f0
ataglich21 <amandatagg@gmail.com>
2025-02-25 17:43:04 -0500

    Added Inventory Test Scene.

--- Assets/Scripts/Scrpt_Player/Scrpt_PlayerInventory/InventorySlot.cs
+++ Assets/Scripts/Scrpt_Player/Scrpt_PlayerInventory/InventorySlot.cs
@@ -7,15 +7,13 @@ public class InventorySlot : MonoBehaviour
 {
     public int quantity;
     public bool isEmpty; 
-    public string itemName; 
-    public Image icon; 
-
+    public Item item; 
+  
     void Awake(){
         //Set all of this to zero when you initiate.
         isEmpty = true; 
         quantity = 0; 
-        itemName = "";
-        icon = null; 
+        item = null; 
 
     }
 
--- Assets/Scripts/Scrpt_Player/Scrpt_PlayerInventory/PlayerInventory.cs
+++ Assets/Scripts/Scrpt_Player/Scrpt_PlayerInventory/PlayerInventory.cs
@@ -8,21 +8,48 @@ public class PlayerInventory : MonoBehaviour
     InventorySlot[] inventory; 
     int inventorySize = 40; 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     void AddItem(Item item){
-        //for(int i = 0; i < inventory.Count; i++){
-        //
-        // }
+        foreach (InventorySlot slot in inventory){
+            if(slot.item == item){
+                //Item already exists in inventory, therefore, we increment its amount.
+                slot.quantity++; 
+                return; 
+            }
+        }
+
+        //If we go through entire loop and havent found a match, we simply add it to next free slot.
+           foreach (InventorySlot slot in inventory){
+            if(slot.isEmpty == true){
+                slot.isEmpty = false; 
+                slot.item = item; 
+            }
+        }
 
     }
 
-    void RemoveItem(){
+    void RemoveItem(Item item){
+        foreach (InventorySlot slot in inventory){
+            if(slot.item == item){
+                slot.quantity--; 
 
+                if(slot.quantity == 0){
+                    slot.isEmpty = true; 
+                    slot.item = null;
+                }
+                return; 
+            }
+        }
+
+    }
+
+    void PrintAllItemsToDebug(){
+        foreach (InventorySlot slot in inventory){
+            if(slot.isEmpty == false){
+               Debug.Log(slot.item + " : Quantity " + slot.quantity );
+               
+            }
+        }
     }
 
     InventorySlot[] getInventory(){
